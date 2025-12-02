a1b2c95653c1c2d239de4f6a741dc2f083359d90
mandip-kamaliya <144941685+mandip-kamaliya@users.noreply.github.com>
2025-12-02 19:13:47 +0530

    installed newer v4-core and v4-periphery version

---
+++ src/mocks/MockToken.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {ERC20} from "solmate/src/tokens/ERC20.sol";
+
+contract MockToken is ERC20 {
+    constructor(string memory _name , string memory _symbol) ERC20(_name,symbol,18){
+        _mint(msg.sender,1000000 * 1e18);
+    }
+
+    function mint(address _to , uint256 _amount) external{
+        _mint(_to,_amount);
+    }
+}
