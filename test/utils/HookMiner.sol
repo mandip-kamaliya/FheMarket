83df8c8461e00f0231f48d51eccde196a16eccbb
mandip-kamaliya <144941685+mandip-kamaliya@users.noreply.github.com>
2025-12-10 19:04:57 +0530

    updated hookminer.sol file

--- test/utils/HookMiner.sol
+++ test/utils/HookMiner.sol
@@ -1,37 +1,36 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {Hooks} from "v4-core/libraries/Hooks.sol";
-
 library HookMiner {
+    // 0x3FFF covers all standard v4 flags
+    uint160 constant FLAG_MASK = 0x3FFF; 
+
     function find(
         address deployer,
         uint160 flags,
         bytes memory creationCode,
         bytes memory constructorArgs
     ) internal pure returns (address, bytes32) {
-        address hookAddress;
-        bytes32 salt;
+        bytes32 creationCodeHash = keccak256(abi.encodePacked(creationCode, constructorArgs));
 
-        for (uint256 i = 0; i < 20000; i++) {
-            salt = bytes32(i);
-            hookAddress = computeAddress(deployer, salt, creationCode, constructorArgs);
+        for (uint256 i = 0; i < 5000000; i++) {
+            bytes32 salt = bytes32(i);
+            address hookAddress = computeAddress(deployer, salt, creationCodeHash);
             
-            if (uint160(hookAddress) & Hooks.ALL_HOOK_MASK == flags) {
+            // Strict check
+            if ((uint160(hookAddress) & FLAG_MASK) == flags) {
                 return (hookAddress, salt);
             }
         }
         revert("HookMiner: could not find salt");
     }
 
-    function computeAddress(
-        address deployer,
-        bytes32 salt,
-        bytes memory creationCode,
-        bytes memory constructorArgs
-    ) internal pure returns (address) {
-        bytes32 bytecodeHash = keccak256(abi.encodePacked(creationCode, constructorArgs));
-        bytes32 hash = keccak256(abi.encodePacked(bytes1(0xff), deployer, salt, bytecodeHash));
-        return address(uint160(uint256(hash)));
+    function computeAddress(address deployer, bytes32 salt, bytes32 creationCodeHash) internal pure returns (address) {
+        return address(uint160(uint256(keccak256(abi.encodePacked(
+            bytes1(0xff),
+            deployer,
+            salt,
+            creationCodeHash
+        )))));
     }
 }
